43b202500cf52092f290db854a7a1591744a79c5
Adam Lány <adam.lany@posam.sk>
2021-10-10 15:15:10 +0200

    Final changes

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.IO;
+using static System.Int32;
 
 namespace HomeWork1
 {
@@ -7,15 +7,52 @@ namespace HomeWork1
     {
         static void Main(string[] args)
         {
-            /*foreach (var arg in args)
+            if (args.Length == 0)
             {
-                Console.WriteLine(arg);
-            }*/
-            /*DirectoryInfo directory = new DirectoryInfo(@"C:\Users\lanya\Desktop\Fun\css");
-            GetDirInfo(directory, 0, maxLevel);*/
+                Console.Error.WriteLine("There is no path defined!");
+                return;
+            }
+            
+            int maxRecursion = -1;
+            int sectionSpace = 2;
+            var isTreeSeparation = false;
+            var isColoredOutput = false;
+            
+            try
+            {
+                for (var i = 1; i < args.Length; i++)
+                {
+                    switch (args[i])
+                    {
+                        case "/r" :
+                            maxRecursion = Parse(args[++i]);
+                            Console.WriteLine($"-> set max directory recursion to: {maxRecursion}");
+                            break;
+                        case "/t" :
+                            isTreeSeparation = true;
+                            if (args.Length <= i + 1 || !TryParse(args[++i], out sectionSpace))
+                            {
+                                sectionSpace = 2;
+                                i--; // cause parameter for indentation wasnt find
+                            }
+                            Console.WriteLine($"-> set tree visualization with indentation: {sectionSpace}");
+                            break;
+                        case "/c" :
+                            isColoredOutput = true;
+                            Console.WriteLine("-> set colored file names");
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Something went wrong! Message -> {e.Message}");
+                return;
+            }
 
-            var command = new LSCommand(-1, true);
-            command.RunCommand(@"C:\Users\lanya\Desktop\Fun\test");
+            var command = new LSCommand(maxRecursion, isTreeSeparation, sectionSpace, isColoredOutput);
+            Console.WriteLine("\n******* Output *******");
+            command.RunCommand(args[0]);
         }
     }
 }
